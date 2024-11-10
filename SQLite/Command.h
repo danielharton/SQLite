23f605dde26e7060b073e6d6882c327e53fe67f3
danielharton <hartondaniel11@gmail.com>
2024-11-10 22:27:01 +0200

    Work on detection but needs more errors checks

--- SQLite/Command.h
+++ SQLite/Command.h
@@ -23,37 +23,67 @@ private:
 	
 public:
 
-	int getCmd(char* fullCommand) {
+	int getCmd(const char* fullCommand) {
 		char* temp=new char[100];
-		
+		temp[0] = '\0';
 
 		for (unsigned int i = 0; i < strlen(fullCommand); i++) {
 			temp[i] = fullCommand[i];
-			
+			temp[i+1] = '\0';
 			if (strcmp(temp, "CREATE TABLE")==0) // if the arrays are identical
 			{
-				return CREATE_TABLE;
+				if (temp[i + 1] == ' ') {
+					delete[] temp;
+					return CREATE_TABLE;
+				}
+				else {
+					cout << "Incorrect command. Try only CREATE TABLE" << endl;
+				}
+				
 			}
 			if (strcmp(temp, "CREATE INDEX") == 0)
 			{
-				return CREATE_INDEX;
+				if (temp[i + 1] == ' ') {
+					delete[] temp;
+					return CREATE_INDEX;
+				}
+				else {
+					cout << "Incorrect command. Try only CREATE INDEX" << endl;
+				}
 			}
 			if (strcmp(temp, "DROP TABLE") == 0)
 			{
-				return DROP_TABLE;
+				if (temp[i + 1] == ' ') {
+					delete[] temp;
+					return DROP_TABLE;
+				}
+				else {
+					cout << "Incorrect command. Try only DROP TABLE" << endl;
+				}
 			}
 			if (strcmp(temp, "DROP INDEX") == 0)
 			{
-				return DROP_INDEX;
+				if (temp[i + 1] == ' ') {
+					delete[] temp;
+					return DROP_INDEX;
+				}
+				else {
+					cout << "Incorrect command. Try only DROP INDEX" << endl;
+				}
 			}
 			if (strcmp(temp, "DISPLAY TABLE") == 0)
 			{
-				return DISPLAY_TABLE;
+				if (temp[i + 1] == ' ') {
+					delete[] temp;
+					return DISPLAY_TABLE;
+				}
 			}
 
 			
 		}
+
 		cout << "Command not found. Try again."<<endl;
+		delete[] temp;
 		return 0;
 	}
 
