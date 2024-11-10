2d4a1086f02ee1660192a82ef413ada0834a8e84
danielharton <hartondaniel11@gmail.com>
2024-11-10 21:40:11 +0200

    Prototype for command detection

--- SQLite/Command.h
+++ SQLite/Command.h
@@ -8,17 +8,57 @@
 
 using namespace std;
 
+enum sqlCmds {
+	CREATE_TABLE=1, CREATE_INDEX, DROP_TABLE, DROP_INDEX, DISPLAY_TABLE
+};
+
+enum crudCmds {
+	INSERT = DISPLAY_TABLE+1, SELECT, UPDATE, DELETE
+};
+
+
 class Command {
 private:
-	string comm;
+	char* comm;
 	
 public:
-	Command() {
+
+	int getCmd(char* fullCommand) {
+		char* temp=new char[100];
 		
+
+		for (unsigned int i = 0; i < strlen(fullCommand); i++) {
+			temp[i] = fullCommand[i];
+			
+			if (strcmp(temp, "CREATE TABLE")==0) // if the arrays are identical
+			{
+				return CREATE_TABLE;
+			}
+			if (strcmp(temp, "CREATE INDEX") == 0)
+			{
+				return CREATE_INDEX;
+			}
+			if (strcmp(temp, "DROP TABLE") == 0)
+			{
+				return DROP_TABLE;
+			}
+			if (strcmp(temp, "DROP INDEX") == 0)
+			{
+				return DROP_INDEX;
+			}
+			if (strcmp(temp, "DISPLAY TABLE") == 0)
+			{
+				return DISPLAY_TABLE;
+			}
+
+			
+		}
+		cout << "Command not found. Try again."<<endl;
+		return 0;
 	}
-	~Command() {
-		
-	}
+
+
+	
 
 };
 
