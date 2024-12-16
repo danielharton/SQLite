031740f1ad75678e42bbb55b05140fb349628b51
danielharton <hartondaniel11@gmail.com>
2024-12-16 22:01:41 +0200

    Created Column Class

--- SQLite/Command.h
+++ SQLite/Command.h
@@ -105,6 +105,85 @@ public:
 	
 
 };
+class Column {
+
+private:
+	char* tableName;
+	unsigned int colSize;
+	char* colName;
+	char* colType;
+	char* defValue;
+public:
+	void setTableName(const char* tableName) {
+		if (tableName == nullptr) {
+			throw "The param of setTableName method can't be nullptr";
+		}
+		this->tableName = new char[strlen(tableName) + 1];
+		strcpy_s(this->tableName, strlen(tableName)+1, tableName);
+	}
+	void setColName(const char* colName) {
+		if (colName == nullptr) {
+			throw "The param of setColName method can't be nullptr";
+		}
+		this->colName = new char[strlen(colName) + 1];
+		strcpy_s(this->colName, strlen(colName) + 1, colName);
+	}
+	char* getColName() {
+		char* tmp = new char[strlen(this->colName) + 1];
+		strcpy_s(tmp, strlen(this->colName)+1, this->colName);
+		return tmp;
+	}
+	void setColType(const char* colType) {
+		
+		if (colType== nullptr &&
+			strcmp(colType, "Integer") == 0 
+			&& strcmp(colType, "Float") == 0
+			&& strcmp(colType, "Text") == 0) {
+			throw "Invalid type. The param of setType should not be nullptr, Integer,Float or Text";
+		}
+		this->colType = new char[strlen(colType)+1];
+		strcpy_s(this->colType, strlen(colType)+1, colType);
+
+	}
+	char* getType() {
+		char* tmp = new char[strlen(this->colType) + 1];
+		strcpy_s(tmp, strlen(this->colType)+1, this->colType);
+		return tmp;
+	}
+	char* setDefValue(const char* defValue) {
+		if (defValue == nullptr) {
+			throw "The param of setDefValue method can't be nullptr";
+		}
+		this->defValue = new char[strlen(defValue) + 1];
+		strcpy_s(this->defValue, strlen(defValue) + 1, defValue);
+	}
+	char* getDefValue() {
+		char* tmp = new char[strlen(this->defValue) + 1];
+		strcpy_s(tmp, strlen(tmp), this->defValue);
+		return tmp;
+	}
+	void fillColumn(const char* tableName, const char* colName, const int& colSize, const char* colType, char* defValue) {
+		this->setColName(colName);
+		this->setTableName(tableName);
+		this->setColType(colType);
+		this->setDefValue(defValue);
+		this->setColSize(colSize);
+
+
+	}
+	unsigned int getColSize() {
+		return this->colSize;
+	}
+	void setColSize(int colSize) {
+		if (colSize < 0) {
+			throw "The param of setColSize can't accept negative value. Column size must be positive";
+		}
+		this->colSize = colSize;
+	}
+
+};
+
+
 
 Command cmd;
 
---
+++ SQLite/sql.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "sqlhead/createtable.h"
+#include "sqlhead/createindex.h"
+#include "sqlhead/droptable.h"
+#include "sqlhead/dropindex.h"
+#include "sqlhead/displaytable.h"
+#include "sqlhead/insertinto.h"
+#include "sqlhead/deletefrom.h"
+#include "sqlhead/select.h"
+#include "sqlhead/update.h"
